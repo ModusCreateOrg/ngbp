e9e31620e4d5a0b78977f9f427cfe6ca369df7c3
Aaron Smith <ansmith@gmail.com>
2014-07-09 13:38:11 -0400

    Moved Karma from Firefox to PhantomJS, which should be sufficient for unit tests

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -17,7 +17,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-phantomjs-launcher' ],
     preprocessors: {},
 
     /**
@@ -52,7 +52,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'PhantomJS'
     ]
   });
 };
